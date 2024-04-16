f447fc9684983211900fec389ff7a482a2d74dd5
m5kro <zhu.edric@gmail.com>
2024-04-15 21:22:30 -0700

    Ask which API type to use

--- start.py
+++ start.py
@@ -1,3 +1,13 @@
-from cohere_modules.main import main
+print('Select which API type you want to use:')
+print('1. cohere')
+print('2. openai')
+module_choice = input()
 
-main()
+if module_choice == '1':
+    from cohere_modules.main import main
+    main()
+elif module_choice == '2':
+    from modules.main import main
+    main()
+else:
+    print("Invalid input. Please enter '1' or '2'.")
