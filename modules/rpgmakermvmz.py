f677c4301323daac86b5d2972d95af6f312152fd
DazedAnon <dazedanon@gmail.com>
2025-01-07 13:48:16 -0600

    Fix optional

--- modules/rpgmakermvmz.py
+++ modules/rpgmakermvmz.py
@@ -74,16 +74,16 @@ POSITION = 0
 LEAVE = False
 
 # Dialogue / Scroll / Choices (Main Codes)
-CODE401 = False
-CODE405 = False
-CODE102 = False
+CODE401 = True
+CODE405 = True
+CODE102 = True
 
 # Optional
 CODE101 = False  # Turn this one when names exist in 101
 CODE408 = False  # Warning, translates comments and can inflate costs.
 
 # Variables
-CODE122 = True
+CODE122 = False
 
 # Other
 CODE355655 = False
@@ -988,7 +988,7 @@ def searchCodes(page, pbar, jobList, filename):
 
                     ### \\n<Speaker>
                     nCase = None
-                    regex = r"([\\]+[kKnN][wWcCrRrEe]?[\[<](.*?)[>\]])"
+                    regex = r"([\\]+[kKnN][wWcCrRrEe][\[<](.*?)[>\]])"
                     match = re.search(regex, finalJAString)
 
                     # Set Name
