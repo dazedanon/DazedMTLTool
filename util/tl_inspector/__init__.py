d23db3e43248d40c35a9e3cf4dc40c75ac7c2719
DazedAnon <dazedanon@gmail.com>
2026-06-12 12:37:27 -0500

    Auto find editor

--- util/tl_inspector/__init__.py
+++ util/tl_inspector/__init__.py
@@ -1,5 +1,16 @@
 """TL Inspector install helpers for RPG Maker MV/MZ (Idea: Sakura · Plugin: Kao_SSS)."""
 
-from util.tl_inspector.installer import bundled_plugin_path, detect_engine, install, status, uninstall
+from util.tl_inspector.config import detect_editors, load_config, save_config
+from util.tl_inspector.installer import apply_config, bundled_plugin_path, detect_engine, install, status, uninstall
 
-__all__ = ["bundled_plugin_path", "detect_engine", "install", "status", "uninstall"]
+__all__ = [
+    "apply_config",
+    "bundled_plugin_path",
+    "detect_editors",
+    "detect_engine",
+    "install",
+    "load_config",
+    "save_config",
+    "status",
+    "uninstall",
+]
