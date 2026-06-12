c7161217dd9aab30a6251f5cad288cd6e41fb4fb
DazedAnon <dazedanon@gmail.com>
2026-06-12 12:33:31 -0500

    fix kao

--- util/tl_inspector/__init__.py
+++ util/tl_inspector/__init__.py
@@ -1,4 +1,4 @@
-"""TL Inspector install helpers for RPG Maker MV/MZ (Idea: Sakura · Plugin: kaoss)."""
+"""TL Inspector install helpers for RPG Maker MV/MZ (Idea: Sakura · Plugin: Kao_SSS)."""
 
 from util.tl_inspector.installer import bundled_plugin_path, detect_engine, install, status, uninstall
 
