297e8e8bdf237e25075295c5890840d3d4a72f1e
DazedAnon <dazedanon@gmail.com>
2026-08-02 12:01:06 -0500

    chore(gui): remove unused file manager

--- gui/__init__.py
+++ gui/__init__.py
@@ -1,25 +1,4 @@
-"""
-DazedTL GUI Package
-"""
+"""DazedTL GUI package."""
 
 __version__ = "1.0.0"
 __author__ = "DazedTranslations"
-
-# Package imports
-from .main import DazedMTLGUI
-from .config_tab import ConfigTab
-from .rpgmaker_tab import RPGMakerTab
-from .log_viewer import LogViewer
-from .file_manager import FileManager
-from .workflow_tab import WorkflowTab
-from .evaluation_tab import EvaluationTab
-
-__all__ = [
-    "DazedMTLGUI",
-    "ConfigTab",
-    "RPGMakerTab",
-    "LogViewer",
-    "FileManager",
-    "WorkflowTab",
-    "EvaluationTab",
-]
